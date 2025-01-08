1ab85ee0a00a9d84f7ea1f6ad5224b0380c4aafd
spenning <whimizera@mailbox.org>
2025-01-08 17:03:01 +0100

    megaphone added

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+void printToUpper(std::string string)
+{
+	for (long unsigned int i = 0; i < string.length(); i++)
+	{
+		int upper = toupper(string[i]);
+		char c = static_cast<char>(upper);
+		std::cout << c;
+	}
+}
+int main (int argc, char *argv[])
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			printToUpper(argv[i]);
+		}
+	}
+	std::cout << "\n";
+}
